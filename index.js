6b30d1ef08f3669290ffc35bcab3be5d1bd632e5
kemocone <115481035+vacc0arno@users.noreply.github.com>
2022-12-26 22:53:23 +0200

    Update index.js

--- index.js
+++ index.js
@@ -33,6 +33,17 @@ app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
 });
 
+
+
+
+
+app.get("/croxy", (req, res) => {
+  somx='<iframe src="mypage.html" style="position:fixed; top:0; left:0; bottom:0; right:0; width:100%; height:100%; border:none; margin:0; padding:0; overflow:hidden; z-index:999999;">Your browser doesnt support iframes</iframe>"'
+  res.send(somx);
+});
+
+
+
 // Search page
 app.get("/s", async (req, res) => {
   let query = req.query.q;
